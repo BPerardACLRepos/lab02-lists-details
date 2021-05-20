bbbcf7b02aae55525a3aacacaefe914ae543d9b3
Brandon Perard <brandon.perard@gmail>
2021-05-19 22:11:35 -0700

    add List.jsx container

---
+++ src/containers/List.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { getCharacters } from '../services/avatarApi';
+
+export default class List extends Component {
+    state = {
+        loading: true,
+        characters: [],
+    }
+
+    async componentDidMount() {
+        const characters = await getCharacters();
+        this.setState({
+            loading: false,
+            characters,
+        });
+    }
+
+    render() {
+        const { loading, characters } = this.state;
+
+        return (
+            <>
+                {loading &&
+                    <h2>Loading...</h2>
+                }
+                {!loading &&
+                    <Characters characters={characters} />
+                }
+            </>
+        );
+    }
+}
